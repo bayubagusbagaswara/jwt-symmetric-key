eafbc4ba026c29f0d62c7dccb678e00343ca119c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 21:19:32 +0700

    add service impl TokenServiceImpl

---
+++ src/main/java/com/bayu/jwt/symmetric/service/impl/TokenServiceImpl.java
@@ -0,0 +1,10 @@
+package com.bayu.jwt.symmetric.service.impl;
+
+import com.bayu.jwt.symmetric.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TokenServiceImpl implements TokenService {
+}

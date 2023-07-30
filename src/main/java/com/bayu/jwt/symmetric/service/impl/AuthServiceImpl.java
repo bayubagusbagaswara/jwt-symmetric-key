5fef8a2c991a35d06de83978ffaa71da009de712
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 21:29:22 +0700

    implement method signIn

--- src/main/java/com/bayu/jwt/symmetric/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bayu/jwt/symmetric/service/impl/AuthServiceImpl.java
@@ -9,10 +9,15 @@ import com.bayu.jwt.symmetric.model.User;
 import com.bayu.jwt.symmetric.repository.RoleRepository;
 import com.bayu.jwt.symmetric.repository.UserRepository;
 import com.bayu.jwt.symmetric.service.AuthService;
+import com.bayu.jwt.symmetric.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,6 +34,8 @@ public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public RegistrationResponse registration(RegistrationRequest registrationRequest) {
@@ -57,6 +64,20 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthenticationResponse signIn(LoginRequest request) {
-        return null;
+        try {
+            log.info("Start sign in with username : {}", request.getUsername());
+            Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    request.getUsername(), request.getPassword()
+            ));
+            log.info("Authenticate : {}", authenticate.getName());
+            String token = tokenService.generateToken(authenticate);
+            log.info("Token : {}", token);
+            return AuthenticationResponse.builder()
+                    .token(token)
+                    .build();
+        } catch (AuthenticationException e) {
+            return new AuthenticationResponse(e.getMessage());
+        }
     }
+
 }

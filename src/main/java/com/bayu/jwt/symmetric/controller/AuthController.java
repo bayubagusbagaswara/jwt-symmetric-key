49e3a9f67c4167dfe2cb3b93f9c1f293bb34391d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:51:30 +0700

    add handler registration

--- src/main/java/com/bayu/jwt/symmetric/controller/AuthController.java
+++ src/main/java/com/bayu/jwt/symmetric/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.bayu.jwt.symmetric.controller;
 
+import com.bayu.jwt.symmetric.dto.ApiResponse;
+import com.bayu.jwt.symmetric.dto.RegistrationRequest;
+import com.bayu.jwt.symmetric.dto.RegistrationResponse;
 import com.bayu.jwt.symmetric.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping(path = "/registration")
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registration(@RequestBody RegistrationRequest request) {
+        RegistrationResponse registrationResponse = authService.registration(request);
+        return ResponseEntity.ok()
+                .body(new ApiResponse<>(Boolean.TRUE, "Success", registrationResponse));
+    }
 
 }

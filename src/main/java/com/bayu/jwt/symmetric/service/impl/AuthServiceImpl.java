71e09560f971771895ae7e8bec5dd355069bfb40
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:56:02 +0700

    implement method registration

--- src/main/java/com/bayu/jwt/symmetric/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bayu/jwt/symmetric/service/impl/AuthServiceImpl.java
@@ -4,14 +4,18 @@ import com.bayu.jwt.symmetric.dto.AuthenticationResponse;
 import com.bayu.jwt.symmetric.dto.LoginRequest;
 import com.bayu.jwt.symmetric.dto.RegistrationRequest;
 import com.bayu.jwt.symmetric.dto.RegistrationResponse;
+import com.bayu.jwt.symmetric.model.Role;
+import com.bayu.jwt.symmetric.model.User;
 import com.bayu.jwt.symmetric.repository.RoleRepository;
 import com.bayu.jwt.symmetric.repository.UserRepository;
 import com.bayu.jwt.symmetric.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +29,22 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public RegistrationResponse registration(RegistrationRequest registrationRequest) {
-        return null;
+        Role role = roleRepository.findFirstByAuthority("USER")
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role not found"));
+
+        log.info("Authority : {}", role.getAuthority());
+
+        User user = new User();
+        user.setUsername(registrationRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+        user.addRole(role);
+
+        userRepository.save(user);
+
+        return RegistrationResponse.builder()
+                .success(Boolean.TRUE)
+                .message("Registration successfully with id : " + user.getId())
+                .build();
     }
 
     @Override

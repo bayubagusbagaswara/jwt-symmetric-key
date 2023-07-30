f184aa3ae3f8d8ff786df37d99bcfda428a0a570
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:50:47 +0700

    add method contract signIn

--- src/main/java/com/bayu/jwt/symmetric/service/AuthService.java
+++ src/main/java/com/bayu/jwt/symmetric/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.bayu.jwt.symmetric.service;
 
+import com.bayu.jwt.symmetric.dto.AuthenticationResponse;
+import com.bayu.jwt.symmetric.dto.LoginRequest;
 import com.bayu.jwt.symmetric.dto.RegistrationRequest;
 import com.bayu.jwt.symmetric.dto.RegistrationResponse;
 
@@ -7,4 +9,5 @@ public interface AuthService {
 
     RegistrationResponse registration(RegistrationRequest registrationRequest);
 
+    AuthenticationResponse signIn(LoginRequest request);
 }

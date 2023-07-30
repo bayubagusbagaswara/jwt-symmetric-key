09a0bc20588364573a5df41adaea9e8b695283ad
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:47:19 +0700

    add dto RegistrationResponse

---
+++ src/main/java/com/bayu/jwt/symmetric/dto/RegistrationResponse.java
@@ -0,0 +1,18 @@
+package com.bayu.jwt.symmetric.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationResponse {
+
+    private Boolean success;
+
+    private String message;
+
+}

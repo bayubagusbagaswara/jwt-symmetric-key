8e9bba3fe7302a98da8341d7f9de308ed38a3618
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:50:28 +0700

    add method contract registration

--- src/main/java/com/bayu/jwt/symmetric/service/AuthService.java
+++ src/main/java/com/bayu/jwt/symmetric/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.bayu.jwt.symmetric.service;
 
+import com.bayu.jwt.symmetric.dto.RegistrationRequest;
+import com.bayu.jwt.symmetric.dto.RegistrationResponse;
+
 public interface AuthService {
+
+    RegistrationResponse registration(RegistrationRequest registrationRequest);
+
 }

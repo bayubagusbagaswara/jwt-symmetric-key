2fc01a42c7a0b23db926f72429b779e2b2d8fef0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:29:01 +0700

    initial commit

---
+++ src/test/java/com/bayu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

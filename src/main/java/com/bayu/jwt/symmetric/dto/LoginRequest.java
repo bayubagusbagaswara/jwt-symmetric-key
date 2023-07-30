77902e3be105799c0e39f10d3d35ba7e40a3b690
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:47:59 +0700

    add dto LoginRequest

---
+++ src/main/java/com/bayu/jwt/symmetric/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.bayu.jwt.symmetric.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    private String username;
+
+    private String password;
+
+}

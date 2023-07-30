f2c986aec399f47b62c17bc85b0e6cd1ef80a505
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:45:58 +0700

    add dto ApiResponse

---
+++ src/main/java/com/bayu/jwt/symmetric/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.bayu.jwt.symmetric.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private Boolean success;
+
+    private String message;
+
+    private T data;
+
+}

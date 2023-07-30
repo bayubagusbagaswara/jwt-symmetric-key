2e771ec26d8ff195dbfa1ff7dc09ef7ab3c343c9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:41:09 +0700

    add service contract UserService

---
+++ src/main/java/com/bayu/jwt/symmetric/service/UserService.java
@@ -0,0 +1,12 @@
+package com.bayu.jwt.symmetric.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserService extends UserDetailsService {
+
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+}

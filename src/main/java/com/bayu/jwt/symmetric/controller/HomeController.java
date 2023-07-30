96827f41998d5364aa7e230e8ba2f3f04a2b027a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 21:10:13 +0700

    update url

--- src/main/java/com/bayu/jwt/symmetric/controller/HomeController.java
+++ src/main/java/com/bayu/jwt/symmetric/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.bayu.jwt.symmetric.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api/home")
 public class HomeController {
 
-    @GetMapping(path = "/")
+    @GetMapping
     public String home() {
         return "Hello, tokes is valid";
     }

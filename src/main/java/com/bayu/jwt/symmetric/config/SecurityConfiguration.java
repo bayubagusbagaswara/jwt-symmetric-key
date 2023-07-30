1c54a355a9459e764dd6621774c2ad90c149cdbe
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-30 20:54:23 +0700

    add annotation

--- src/main/java/com/bayu/jwt/symmetric/config/SecurityConfiguration.java
+++ src/main/java/com/bayu/jwt/symmetric/config/SecurityConfiguration.java
@@ -1,4 +1,15 @@
 package com.bayu.jwt.symmetric.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
+
 }

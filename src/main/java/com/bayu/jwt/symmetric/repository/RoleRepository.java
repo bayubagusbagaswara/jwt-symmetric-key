5b3e691eed47b83ac52cbab1cf90ee8cbdfd547d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-02 09:25:27 +0700

    rename parameter

--- src/main/java/com/bayu/jwt/symmetric/repository/RoleRepository.java
+++ src/main/java/com/bayu/jwt/symmetric/repository/RoleRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
-    Optional<Role> findFirstByAuthority(String authorityName);
+    Optional<Role> findFirstByAuthority(String authority);
 }
